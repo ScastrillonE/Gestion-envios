f9cebec2be300f67a6a8654bd316468aec926a5b
SantiagoCE <santiagocastrillon.ep@gmail.com>
2020-08-04 12:36:56 -0500

    sentry add

--- heracles/urls.py
+++ heracles/urls.py
@@ -6,6 +6,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
@@ -15,6 +18,10 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('company.urls')),
     path('', include('notes.urls')),
+    path('sentry-debug/', trigger_error),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
+
+
+
